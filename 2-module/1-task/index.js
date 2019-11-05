c0cddd844542be11a4754bc802c9d2f312dcfd0f
Zvenigorodskaia <work1908@gmail.com>
2019-11-05 22:16:52 +0300

    1th task (2 module)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,18 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
-function clone(obj) {
+const obj = { test: 1 };
+const obj2 = {};
+
+function clone() {
+  for (const key in obj) {
+    obj2[key] = obj[key];
+  }
+
+  obj2.test = 2;
+
+  alert(obj2.test);
+  alert(obj.test);
 }
+
+clone();
