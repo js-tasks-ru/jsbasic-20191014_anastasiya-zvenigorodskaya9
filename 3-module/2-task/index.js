0dfa323ed17f0a97821c9dd653edc357c45cccc8
Zvenigorodskaia <work1908@gmail.com>
2019-12-27 01:23:32 +0300

    Merge branch 'master' of https://github.com/js-tasks-ru/jsbasic-20191014_anastasiya-zvenigorodskaya9
    # Conflicts:
    #	3-module/2-task/index.js

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -6,25 +6,15 @@
 
 
 const inputData = '1, -5.8 или 10, хотя 34 + -5.3 и 73';
-const inputArray = inputData.split(' ');
-
-
-
-
 
 function getMinMax(str) {
   let segments = str.match(/(-|\+)?[\d]+(\.[\d]+)?/ig) || [];
 
   segments = segments.map(seg => parseFloat(seg));
-
   return {
     min: Math.min(...segments),
     max: Math.max(...segments),
   };
 }
 
-
-getMinMax(inputArray);
-
-
-console.log(inputArray); // ожидается [-5.8, 73]
+getMinMax(inputData);
