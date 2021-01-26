90317c22580a10c446ef2d7b1e08eeef4cf2b60f
zvenigorodskaia <work1908@gmail.com>
2021-01-26 23:21:56 +0300

    module 1 - task 3 - ucFirst(str) function

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -3,5 +3,13 @@
  * @param {string} str
  * @returns {string}
  */
+
 function ucFirst(str) {
+  if (str.length > 0) {
+    let firstSymbol = str[0].toUpperCase();
+    let capitalizedStr = firstSymbol + str.slice(1);
+    return capitalizedStr;
+  } else {return '';}
 }
+
+ucFirst('');
