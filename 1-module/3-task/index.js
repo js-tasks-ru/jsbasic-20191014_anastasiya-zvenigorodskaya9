7ab1518b15939bae0ce06b96fb3541c348cfc940
zvenigorodskaia <work1908@gmail.com>
2021-01-27 22:33:30 +0300

    module 1 - task 4 - CheckSpam function

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -7,9 +7,6 @@
 function ucFirst(str) {
   if (str.length > 0) {
     let firstSymbol = str[0].toUpperCase();
-    let capitalizedStr = firstSymbol + str.slice(1);
-    return capitalizedStr;
+    return firstSymbol + str.slice(1);
   } else {return '';}
 }
-
-ucFirst('');
